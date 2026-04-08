dc9d383f844572fdeebf9d241547bb8c599f40f2
hidayat1816 <hidahasanah265@gmail.com>
2026-04-08 13:20:31 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:limit_kuota/main.dart';
+import 'package:praktikum_ppb2_limitkuota_kelompok4b/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
