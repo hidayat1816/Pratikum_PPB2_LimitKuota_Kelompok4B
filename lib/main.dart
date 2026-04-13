21788264646f0b7c324365f6fe7daef7456b0a06
hidayat1816 <hidahasanah265@gmail.com>
2026-04-13 11:25:48 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Limit Kuota',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
