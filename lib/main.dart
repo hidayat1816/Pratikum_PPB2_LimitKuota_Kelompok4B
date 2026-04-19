1628088dc5cd636eb56b3854fa5988eda0e5c927
julitaliita <julitajulita715@gmail.com>
2026-04-18 20:37:12 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,24 @@ import 'core/services/notification_service.dart';
 import 'features/home/ui/home_page.dart';
 // ignore: unused_import
 import 'features/home/ui/sidebar.dart';
+import 'package:provider/provider.dart';
+import 'core/providers/profile_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await NotificationService.init(); // 🔥 WAJIB
 
-  runApp(MyApp());
+  // 🔥 TAMBAHAN
+  final profileProvider = ProfileProvider();
+  await profileProvider.loadProfile(); // load data tersimpan
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => profileProvider,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
